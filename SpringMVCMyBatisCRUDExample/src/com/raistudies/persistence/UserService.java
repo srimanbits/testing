8c16290b030eaeb93a5b21fad7baf4c970c419e8
srimanbits <srimanbits23@gmail.com>
2015-09-04 17:53:45 +0530

    ccc
    ddddddddddddddd

---
+++ SpringMVCMyBatisCRUDExample/src/com/raistudies/persistence/UserService.java
@@ -0,0 +1,15 @@
+package com.raistudies.persistence;
+
+import java.util.List;
+
+import com.raistudies.domain.User;
+
+public interface UserService {
+	
+	public void saveUser(User user);
+	public void updateUser(User user);
+	public void deleteUser(String id);
+	public List<User> getAllUser();
+	public int getMaxId();
+	
+}
